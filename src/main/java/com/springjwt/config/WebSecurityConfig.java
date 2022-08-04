e2176f832ce87de12845bc3608cfebf3052dd87f
Amit Chavda <amitchavda9879@gmail.com>
2022-08-04 11:39:46 +0530

    Upgraded Spring Security to 2.7.0+ which is component based configuration. This will solve #2 issue.

--- src/main/java/com/springjwt/config/WebSecurityConfig.java
+++ src/main/java/com/springjwt/config/WebSecurityConfig.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.Bean;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,27 +20,26 @@ import com.springjwt.filter.CustomAuthFilter;
 import com.springjwt.filter.JwtAuthFilter;
 
 @EnableWebSecurity
-public class WebSecurityConfig {
-
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private UserService customUserDetailsService;
-    private JwtAuthFilter authFilter;
-    private CustomAuthFailure authFailure;
     private AuthenticationConfiguration authenticationConfiguration;
-
-    public WebSecurityConfig(UserService customUserDetailsService, JwtAuthFilter authFilter, CustomAuthFailure authFailure) {
+    private JwtAuthFilter jwtAuthFilter;
+    public WebSecurityConfig(UserService customUserDetailsService, AuthenticationConfiguration authenticationConfiguration, JwtAuthFilter jwtAuthFilter) {
         this.customUserDetailsService = customUserDetailsService;
-        this.authFilter = authFilter;
-        this.authFailure = authFailure;
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authenticationProvider(authenticationProvider());
+        http.authenticationManager(authenticationManager());
+
         http.csrf().disable().cors().disable();
 
         http.authorizeRequests()
                 .antMatchers("/login", "/forgotPassword", "/resetPassword", "/register", "/loginError").permitAll()
-                .antMatchers("/loginError").permitAll()
                 .anyRequest().authenticated();
 
         http.sessionManagement()
@@ -52,15 +49,13 @@ public class WebSecurityConfig {
                 .usernameParameter("username")
                 .passwordParameter("password")
                 .loginPage("/login")
-                .failureHandler(authFailure);
+                .failureHandler(new CustomAuthFailure());
 
         http.addFilter(new CustomAuthFilter(authenticationManager()));
-
-        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
@@ -78,6 +73,10 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
 }
