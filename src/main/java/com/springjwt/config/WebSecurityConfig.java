022d7f8557edd8c5ca1f11d7367adc146b816f2a
Amit Chavda <amitchavda9879@gmail.com>
2022-08-04 11:21:01 +0530

    dependencies upgraded

--- src/main/java/com/springjwt/config/WebSecurityConfig.java
+++ src/main/java/com/springjwt/config/WebSecurityConfig.java
@@ -7,40 +7,37 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.springjwt.filter.CustomAuthFailure;
 import com.springjwt.filter.CustomAuthFilter;
 import com.springjwt.filter.JwtAuthFilter;
 
 @EnableWebSecurity
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
 
     private UserService customUserDetailsService;
     private JwtAuthFilter authFilter;
     private CustomAuthFailure authFailure;
+    private AuthenticationConfiguration authenticationConfiguration;
 
-    public SecurityConfigurer(UserService customUserDetailsService, JwtAuthFilter authFilter, CustomAuthFailure authFailure) {
+    public WebSecurityConfig(UserService customUserDetailsService, JwtAuthFilter authFilter, CustomAuthFailure authFailure) {
         this.customUserDetailsService = customUserDetailsService;
         this.authFilter = authFilter;
         this.authFailure = authFailure;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable();
 
         http.authorizeRequests()
@@ -60,9 +57,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         http.addFilter(new CustomAuthFilter(authenticationManager()));
 
         http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
-    DaoAuthenticationProvider authenticationProvider() {
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(customUserDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
@@ -75,8 +76,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
